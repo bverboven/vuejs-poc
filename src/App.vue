2b68643d0578cae697c0eac4e22a24ad79d5c008
B. Verboven <bramverboven@hotmail.com>
2020-02-23 23:35:04 +0100

    external css

--- src/App.vue
+++ src/App.vue
@@ -10,33 +10,3 @@
     <router-view />
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  padding-top: 1rem;
-
-  h1 {
-    font-size: 1.25rem;
-  }
-  h2 {
-    font-size: 1.1rem;
-  }
-  h2,
-  h2 a {
-    color: #333;
-  }
-  .vue-logo {
-    height: 1.5rem;
-    float: left;
-    margin-right: 0.5rem;
-  }
-
-  header {
-    border: 2px dotted transparent;
-
-    .nav-link {
-      text-transform: capitalize;
-    }
-  }
-}
-</style>
